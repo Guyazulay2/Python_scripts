8d4e085d854619b92bf2e473e3b1152dac78d3d7
GuyAzulay <59178662+Guyazulay2@users.noreply.github.com>
2021-11-15 12:18:28 +0200

    Update socket.py

--- socket.py
+++ socket.py
@@ -1,4 +1,5 @@
 import socket
+import argparse
 
 def check_port(ip, port):
     TCPsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,8 +13,8 @@ def check_port(ip, port):
         return False
 
 def main():
-    server1, server2, server3, server4 = '10.11.12.22', '10.11.12.23', '10.11.12.24', '10.11.12.25'
-    ips = [server1, server2, server3, server4]
+    # server1, server2, server3, server4 = '10.11.12.22', '10.11.12.23', '10.11.12.24', '10.11.12.25'
+    ips = [args.ip1,args.ip2,args.ip3,args.ip4]
     port1, port2, port3, port4 = 3044, 3044, 4044, 4044
     ports = [port1, port2, port3, port4]
 
@@ -25,5 +26,20 @@ def main():
         print(f"Server{i} ({ip}) --> MainServer [{'OK' if res else 'Error'}]")
         i += 1
 
-if __name__ == "__main__":
-    main()
+def args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-ip1', type=str, help='Enter IP-1')
+    parser.add_argument('-ip2', type=str, help='Enter IP-2')
+    parser.add_argument('-ip3', type=str, help='Enter IP-3')
+    parser.add_argument('-ip4', type=str, help='Enter IP-4')
+    
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = args()
+    if not args.ip1 :
+        print("You have to Enter at least 1 IP Adress args ! See -h")
+    else:
+        main()
